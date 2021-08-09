ad0cce1a3c7d73b777edba7bf15ad3e3f1cc74f5
MonkDao <danil.alexsandr@gmail.com>
2021-08-09 16:51:46 +0300

    inut

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        String name = "alex";
         int i = 10;
         System.out.println(i);
     }
---
+++ src/Veas.java
@@ -0,0 +1,5 @@
+public class Veas {
+    public static void main(String[] args) {
+        int a = 12;
+    }
+}
